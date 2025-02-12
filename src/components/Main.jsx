746bbcab6a1cc16cd8098c9f72f9d90607f0acd1
Salvatore <workstudioms@gmail.com>
2025-02-12 17:09:48 +0100

    Main content and basic css , done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,40 @@ const Main = () => {
     return (
         <>
             <main>
-                <h2>Main Content</h2>
+
+                <div id="main_content">
+                    <h2> → Content goes here ← </h2>
+                </div>
+
+                <div id="secondary_content">
+                    <ul>
+                        <li>
+                            <img src="./img/buy-comics-digital-comics.png" />
+                            <a href="#">DIGITAL COMICS</a>
+                        </li>
+
+                        <li>
+                            <img src="./img/buy-comics-merchandise.png" />
+                            <a href="#">DC MERCHANDISE</a>
+                        </li>
+
+                        <li>
+                            <img src="./img/buy-comics-subscriptions.png" />
+                            <a href="#">SUBSCRIPTIONS</a>
+                        </li>
+
+                        <li>
+                            <img src="./img/buy-comics-shop-locator.png" />
+                            <a href="#">COMICS SHOP LOCATOR</a>
+                        </li>
+
+                        <li>
+                            <img src="./img/buy-dc-power-visa.svg" />
+                            <a href="#">DC POWER VISA</a>
+                        </li>
+                    </ul>
+                </div>
+
             </main>
         </>
     )
